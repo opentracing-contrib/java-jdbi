61e3457f5df7b6be8994d689a2b7508588b86f4a
Sergei Malafeev <sergeymalafeev@gmail.com>
2018-03-13 20:39:48 +0800

    - change release process - add license headers - use h2 for test - update README
    Signed-off-by: Sergei Malafeev <sergeymalafeev@gmail.com>

--- src/test/java/io/opentracing/contrib/jdbi/OpenTracingCollectorTest.java
+++ src/test/java/io/opentracing/contrib/jdbi/OpenTracingCollectorTest.java
@@ -1,62 +1,64 @@
+/*
+ * Copyright 2016-2018 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package io.opentracing.contrib.jdbi;
 
 import io.opentracing.Span;
 import io.opentracing.Tracer;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.skife.jdbi.v2.*;
+import org.skife.jdbi.v2.DBI;
+import org.skife.jdbi.v2.Handle;
+import org.skife.jdbi.v2.Query;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.TimingCollector;
 
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-// REQUIRES: a mysql database running on localhost.
 public class OpenTracingCollectorTest {
-    private static DBI getLocalDBI() {
-        return getLocalDBI("");
-    }
-    private static DBI getLocalDBI(String dbName) {
-        return new DBI("jdbc:mysql://127.0.0.1/" + dbName, "root", "");
+    private DBI dbi;
+    private Handle handle;
+
+    @Before
+    public void before() {
+        dbi = new DBI("jdbc:h2:mem:dbi", "sa", "");
+        handle = dbi.open();
+        handle.execute("CREATE TABLE accounts (id BIGINT AUTO_INCREMENT, PRIMARY KEY (id))");
     }
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        {
-            DBI dbi = getLocalDBI();
-            Handle handle = dbi.open();
-            handle.execute("CREATE DATABASE IF NOT EXISTS _jdbi_test_db");
-        }
-        {
-            DBI dbi = getLocalDBI("_jdbi_test_db");
-            Handle handle = dbi.open();
-            handle.execute("CREATE TABLE IF NOT EXISTS accounts (id BIGINT AUTO_INCREMENT, PRIMARY KEY (id))");
-        }
-    }
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        DBI dbi = getLocalDBI();
-        Handle handle = dbi.open();
-        handle.execute("DROP DATABASE _jdbi_test_db");
+    @After
+    public void after() {
+        handle.close();
     }
 
     @Test
     public void testParentage() {
         MockTracer tracer = new MockTracer();
-        DBI dbi = getLocalDBI("_jdbi_test_db");
         dbi.setTimingCollector(new OpenTracingCollector(tracer));
 
         // The actual JDBI code:
-        {
-            Handle handle = dbi.open();
+        try (Handle handle = dbi.open()) {
             Tracer.SpanBuilder parentBuilder = tracer.buildSpan("parent span");
             Span parent = parentBuilder.start();
-            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM accounts");
+            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM " +
+                    "accounts");
             OpenTracingCollector.setParent(statement, parent);
 
             // A Span will be created automatically and will reference `parent`.
@@ -75,11 +77,10 @@ public class OpenTracingCollectorTest {
     }
 
     @Test
-    // Requires a mysql database running on localhost.
     public void testDecorations() {
         MockTracer tracer = new MockTracer();
-        DBI dbi = getLocalDBI("_jdbi_test_db");
-        dbi.setTimingCollector(new OpenTracingCollector(tracer, new OpenTracingCollector.SpanDecorator() {
+        dbi.setTimingCollector(new OpenTracingCollector(tracer, new OpenTracingCollector
+                .SpanDecorator() {
             @Override
             public String generateOperationName(StatementContext ctx) {
                 return "custom name";
@@ -92,9 +93,9 @@ public class OpenTracingCollectorTest {
         }));
 
         // The actual JDBI code:
-        {
-            Handle handle = dbi.open();
-            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM accounts");
+        try (Handle handle = dbi.open()) {
+            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM " +
+                    "accounts");
             List<Map<String, Object>> results = statement.list();
         }
 
@@ -106,14 +107,13 @@ public class OpenTracingCollectorTest {
     }
 
     @Test
-    // Requires a mysql database running on localhost.
     public void testActiveSpanSource() {
         MockTracer tracer = new MockTracer();
-        DBI dbi = getLocalDBI("_jdbi_test_db");
 
         Tracer.SpanBuilder parentBuilder = tracer.buildSpan("active span");
         final Span activeSpan = parentBuilder.start();
-        dbi.setTimingCollector(new OpenTracingCollector(tracer, new OpenTracingCollector.ActiveSpanSource() {
+        dbi.setTimingCollector(new OpenTracingCollector(tracer, new OpenTracingCollector
+                .ActiveSpanSource() {
             @Override
             public Span activeSpan(StatementContext ctx) {
                 return activeSpan;
@@ -121,9 +121,9 @@ public class OpenTracingCollectorTest {
         }));
 
         // The actual JDBI code:
-        {
-            Handle handle = dbi.open();
-            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM accounts");
+        try (Handle handle = dbi.open()) {
+            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM " +
+                    "accounts");
             List<Map<String, Object>> results = statement.list();
         }
         activeSpan.finish();
@@ -142,21 +142,22 @@ public class OpenTracingCollectorTest {
     @Test
     public void testChainsToNext() {
         MockTracer tracer = new MockTracer();
-        DBI dbi = getLocalDBI("_jdbi_test_db");
         Tracer.SpanBuilder parentBuilder = tracer.buildSpan("active span");
 
         class TestTimingCollector implements TimingCollector {
             boolean called = false;
+
             @Override
             public void collect(long l, StatementContext statementContext) {
                 called = true;
             }
-        };
+        }
 
         TestTimingCollector subject = new TestTimingCollector();
 
         final Span activeSpan = parentBuilder.start();
-        dbi.setTimingCollector(new OpenTracingCollector(tracer, OpenTracingCollector.SpanDecorator.DEFAULT,
+        dbi.setTimingCollector(new OpenTracingCollector(tracer, OpenTracingCollector
+                .SpanDecorator.DEFAULT,
                 new OpenTracingCollector.ActiveSpanSource() {
                     @Override
                     public Span activeSpan(StatementContext ctx) {
@@ -167,12 +168,11 @@ public class OpenTracingCollectorTest {
         );
 
         // The actual JDBI code:
-        {
-            Handle handle = dbi.open();
-            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM accounts");
+        try (Handle handle = dbi.open()) {
+            Query<Map<String, Object>> statement = handle.createQuery("SELECT COUNT(*) FROM " +
+                    "accounts");
             List<Map<String, Object>> results = statement.list();
         }
-
         activeSpan.finish();
 
         assertTrue(subject.called);
