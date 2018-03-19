9a4093b675580ab7f699ac8123e9ba6b98593996
Yair Galler <yair@next-insurance.com>
2018-03-19 14:50:52 +0200

    Remove unused suppression annotation

--- src/main/java/io/opentracing/contrib/jdbi/OpenTracingCollector.java
+++ src/main/java/io/opentracing/contrib/jdbi/OpenTracingCollector.java
@@ -65,7 +65,6 @@ public class OpenTracingCollector implements TimingCollector {
      * @param next   a timing collector to "chain" to. When collect is called on
      *               this TimingCollector, collect will also be called on 'next'
      */
-    @SuppressWarnings("unused")
     public OpenTracingCollector(Tracer tracer, TimingCollector next) {
         this(tracer, SpanDecorator.DEFAULT, null, next);
     }
